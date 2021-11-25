5b8be9bf506415817f8a4d066819ce4085920d16
Marco Villegas <marcovillegascampos@gmail.com>
2021-11-24 22:53:51 -0600

    adding files

---
+++ GroupChat/server.js
@@ -0,0 +1,26 @@
+const express = require( 'express' );
+const app = express();
+
+app.use(express.static(__dirname + "/public"));
+
+const server = app.listen(8888);
+
+const io = require( 'socket.io' )( server );
+const chatlog=[];
+
+io.on( 'connection', function( socket ){
+    //console.log( socket );
+    console.log( "Someone just connected!" );
+
+
+    socket.on( 'general', function( data ){
+        chatlog.push(data);
+        
+        io.sockets.emit( 'listenAll', {message: "Broadcast message"} )
+    });
+
+    socket.on( 'sendMessage', function( data ){
+        io.sockets.emit( 'sendAll', data );
+    });
+    
+});
